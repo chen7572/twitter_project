8ce0a3d6ae9e2ac8b7b84f5e9e6b5420ddd15871
chen7572 <carachenchen@gmail.com>
2017-09-26 23:14:03 -0400

    Search tweets within a time frame

--- extract_tweets.R
+++ extract_tweets.R
@@ -1,5 +1,11 @@
 library("twitteR")
 library("ROAuth")
+
 setup_twitter_oauth(consumer_key = 'cVBUluS3IOoimaTPNIdImY3Mb',consumer_secret = '5wLp0iX2BLKSzTr2cEidCbetGqyrvop6V0bdFHzoD1lIj292C4',access_token = '386134133-yo24CXeFMGISwW66KD4tiYAnai7n2iTvpDIi7r9i',access_secret = '5uCJ5eeqaXPjPxEY3zvepvM84eiVjaCOa5OdCf6bLEaZ4')
+
 harvey <- searchTwitter("#Harvey",n=3000,lang="en")
 irma <- searchTwitter("#Irma",n=3000,lang="en")
+
+harvey2 <- searchTwitter("#Harvey",n=3000,lang="en",since='2017-08-17',until = '2017-08-24')
+harvey3 <- searchTwitter("#Harvey",n=3000,lang="en",since='2017-08-17',resultType = 'recent')
+
