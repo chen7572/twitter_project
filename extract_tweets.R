7ef3b82244937f11c765c7690a48edf6ff8fdd59
chen7572 <carachenchen@gmail.com>
2017-09-26 22:41:54 -0400

    Practice extracting tweets.

---
+++ extract_tweets.R
@@ -0,0 +1,5 @@
+library("twitteR")
+library("ROAuth")
+setup_twitter_oauth(consumer_key = 'cVBUluS3IOoimaTPNIdImY3Mb',consumer_secret = '5wLp0iX2BLKSzTr2cEidCbetGqyrvop6V0bdFHzoD1lIj292C4',access_token = '386134133-yo24CXeFMGISwW66KD4tiYAnai7n2iTvpDIi7r9i',access_secret = '5uCJ5eeqaXPjPxEY3zvepvM84eiVjaCOa5OdCf6bLEaZ4')
+harvey <- searchTwitter("#Harvey",n=3000,lang="en")
+irma <- searchTwitter("#Irma",n=3000,lang="en")
